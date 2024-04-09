1fb7ec23fa31a959f9b81c19eb885ed1b70b68fb
Honey608 <1206136081@qq.com>
2024-04-09 21:05:25 +0800

    环境准备

---
+++ src/main/java/org/example/pojo/Emp.java
@@ -0,0 +1,14 @@
+package org.example.pojo;
+
+import java.time.LocalDateTime;
+
+public class Emp {
+    private Integer id;
+    private String username;
+    private Integer password;
+    private short gender;
+    private String image;
+    private short job;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
